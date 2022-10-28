70c08b1be1e12c32cfc01c3b5229d089719ccc6d
Abduljawad Ibrahim <jawadrumah@gmail.com>
2022-10-28 10:51:34 +0100

    StatisticController

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pipeline;
+use App\Models\Company;
+use App\Models\pipelineType;
 
 class StatisticController extends Controller
 {
-    //
+    function pipelineCount()
+    {
+        $count = Pipeline::all()->count();
+
+        return response()->json($count, 200);
+    }
+
+    function pipelineTypeCount()
+    {
+        $count = PipelineType::all()->count();
+
+        return response()->json($count, 200);
+    }
+
+    function companyCount()
+    {
+        $count = Company::all()->count();
+
+        return response()->json($count, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatisticController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/auth/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::get('/auth/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+
+
 Route::apiResource('pipeline', App\Http\Controllers\PipelineController::class);
 Route::apiResource('pipeline-type', App\Http\Controllers\PipelineTypeController::class);
 Route::apiResource('pipeline-route', App\Http\Controllers\PipelineROUTEController::class);
 Route::apiResource('company', App\Http\Controllers\CompanyController::class);
+Route::get('/type-count', [StatisticController::class, 'pipelineTypeCount']);
+Route::get('/pipeline-count', [StatisticController::class, 'pipelineTypeCount']);
+Route::get('/company-count', [StatisticController::class, 'pipelineTypeCount']);
+
+Route::group(['middleware' => 'auth:sanctum',], function (){
+
+});
