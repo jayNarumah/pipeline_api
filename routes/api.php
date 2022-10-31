76d67e97588e3f94f74cb7cdfd7afa13eb7a61c7
Abduljawad Ibrahim <jawadrumah@gmail.com>
2022-10-31 14:40:46 +0100

    Auth routes added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StatisticController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,17 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/auth/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::get('/auth/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/logout', [AuthController::class, 'logout']);
 
 
 Route::apiResource('pipeline', App\Http\Controllers\PipelineController::class);
 Route::apiResource('pipeline-type', App\Http\Controllers\PipelineTypeController::class);
-Route::apiResource('pipeline-route', App\Http\Controllers\PipelineROUTEController::class);
+Route::apiResource('pipeline-route', App\Http\Controllers\PipelineRouteController::class);
 Route::apiResource('company', App\Http\Controllers\CompanyController::class);
+
 Route::get('/type-count', [StatisticController::class, 'pipelineTypeCount']);
-Route::get('/pipeline-count', [StatisticController::class, 'pipelineTypeCount']);
-Route::get('/company-count', [StatisticController::class, 'pipelineTypeCount']);
+Route::get('/pipeline-count', [StatisticController::class, 'pipelineCount']);
+Route::get('/company-count', [StatisticController::class, 'companyCount']);
 
 Route::group(['middleware' => 'auth:sanctum',], function (){
 
